48faaba98ed5af293af8040f4c8add42883c5b17
treepoint <owner-of-all-real@ya.ru>
2023-02-11 17:23:30 +0500

    some basic fixes

--- src/components/MainHeader.vue
+++ src/components/MainHeader.vue
@@ -1,29 +1,32 @@
 <template>
     <div class="footer_and_header fixed">
         <div class="header">
-            <a class="logo_block" href="https://avetemp.ru" rel="noopener">
-                <img alt="AveTemp Main Screen" class="logo" src="../assets/logo.png"/>
-                <div class="name">AveTemp</div>
-            </a>
-            <div class="header_menu">
-                <ul>
-                    <li>
-                        <a href="#about" rel="noopener">{{ $t('about_headline') }}</a>
-                    </li>
-                    <li>
-                        <a href="#features" rel="noopener">{{ $t('features_headline') }}</a>
-                    </li>
-                    <li>
-                        <a href="#support" rel="noopener">{{ $t('support_headline') }}</a>
-                    </li>
-                    <li>
-                        <a href="#changelog" rel="noopener">{{ $t('changelog_headline') }}</a>
-                    </li>
-                </ul>
+            <div class="header-inner">
+                <a class="logo_block" href="https://avetemp.ru" rel="noopener">
+                    <img alt="AveTemp Main Screen" class="logo" src="../assets/logo.png"/>
+                    <div class="name">AveTemp</div>
+                </a>
+                <div class="header_menu">
+                    <ul>
+                        <li>
+                            <a href="#about" rel="noopener">{{ $t('about_headline') }}</a>
+                        </li>
+                        <li>
+                            <a href="#features" rel="noopener">{{ $t('features_headline') }}</a>
+                        </li>
+                        <li>
+                            <a href="#support" rel="noopener">{{ $t('support_headline') }}</a>
+                        </li>
+                        <li>
+                            <a href="#changelog" rel="noopener">{{ $t('changelog_headline') }}</a>
+                        </li>
+                    </ul>
+                </div>
+
+                <div class="spacer"></div>
+                <LanguageSelect/>
+                <a class="btn btn-danger" :href="download_link" role="button">{{ $t('download_button') }}</a>
             </div>
-            <div class="spacer"></div>
-            <LanguageSelect/>
-            <a class="btn btn-danger" :href="download_link" role="button">{{ $t('download_button') }}</a>
         </div>
     </div>
 </template>
