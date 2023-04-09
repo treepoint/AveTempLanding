7141660407d6f1d3615ceda2fed949db8ce6a56b
treepoint <owner-of-all-real@ya.ru>
2023-04-09 19:12:40 +0500

    added metrics

--- pages/index.js
+++ pages/index.js
@@ -45,6 +45,45 @@ export default function Home({ dir }) {
         <link rel="alternate" href={host} hrefLang="x-default" />
         <link rel="alternate" href={host+"/en"} hrefLang="en" />
         <link rel="alternate" href={host+"/ru"} hrefLang="ru" />
+        
+        {/* Yandex Metrika */}
+        <script
+            dangerouslySetInnerHTML={{
+              __html: `
+                (function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
+                m[i].l=1*new Date();
+                for (var j = 0; j < document.scripts.length; j++) {if (document.scripts[j].src === r) { return; }}
+                k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})
+                (window, document, "script", "https://cdn.jsdelivr.net/npm/yandex-metrica-watch/tag.js", "ym");
+          
+                ym(91976312, "init", {
+                    clickmap:true,
+                    trackLinks:true,
+                    accurateTrackBounce:true,
+                    webvisor:true
+                });
+              `,
+            }}
+          />
+        <noscript>
+          <div>
+            <img src="https://mc.yandex.ru/watch/91976312" style={{ position:'absolute', left:'-9999px' }} alt="" />
+          </div>
+        </noscript>
+
+        {/* Google tag (gtag.js) */}
+        <script async src="https://www.googletagmanager.com/gtag/js?id=G-1D8PFXBZ8V"></script>
+        <script
+            dangerouslySetInnerHTML={{
+              __html: `window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+        
+              gtag('config', 'G-1D8PFXBZ8V');
+              `,
+            }}
+          />
+
       </Head>
 
       <Header styles={styles} locales = {[...locales]}/>
