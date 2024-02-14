213a2d6bd255ab0923b7291517229d44dce4cb5a
treepoint <owner-of-all-real@ya.ru>
2024-02-15 01:04:06 +0500

    download window + pleanty of fixes

--- _globals.js
+++ _globals.js
@@ -1,3 +1,4 @@
-download_link = "https://github.com/treepoint/AveTemp/releases/download/1.4.7/AveTemp.exe";
+avetemp_download_link = "https://github.com/treepoint/AveTemp/releases/download/1.4.7/AveTemp.exe";
+avewall_download_link = "https://github.com/treepoint/AveWall/releases/download/0.3/AveWall.exe";
 host = "https://avetemp.ru";
 
