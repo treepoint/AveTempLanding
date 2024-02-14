4aa4889981ae878d5f10f97d54221bc94fffca59
treepoint <owner-of-all-real@ya.ru>
2024-02-15 01:04:06 +0500

    download window + pleanty of fixes

--- components/DownloadWindow/DownloadWindow.js
+++ components/DownloadWindow/DownloadWindow.js
@@ -3,6 +3,8 @@ import ModalWindow from "../../elements/ModalWindow/ModalWindow"
 import styles from './DownloadWindow.module.scss'
 import Link from "next/link"
 import Script from "next/script"
+import Image from "next/image.js"
+
 import { useIntl } from "react-intl"
 import { useState, useEffect } from "react"
 import "../../_globals.js"
@@ -19,7 +21,8 @@ export default function DownloadWindow() {
     const link_will_be_available = intl.formatMessage({ id: "link_will_be_available" });
     const seconds_text = intl.formatMessage({ id: "seconds..." });
     const download_disclaimer = intl.formatMessage({ id: "download_disclaimer" });
-    const your_link = intl.formatMessage({ id: "your_link" });
+    const avetemp_download_description = intl.formatMessage({ id: "avetemp_download_description" });
+    const avewall_download_description = intl.formatMessage({ id: "avewall_download_description" });
 
     const isOpened = useSelector(selectIsOpened);
 
@@ -35,14 +38,38 @@ export default function DownloadWindow() {
 
     return (
         <ModalWindow title={download}>
-            {
-                timer ? 
-                <p>{link_will_be_available}{timer}{seconds_text}</p> 
-                : 
-                <div>
-                    <p>{download_disclaimer} {your_link} <Link href={download_link}>{download}</Link></p>
+            <div className={styles.catalog_wrapper}>
+                <div className={styles.item}>
+                    <div className={styles.image}>
+                        <Image alt={'AveTemp fullsize logo'} 
+                            src={'/images/avetemp_logo.png'} 
+                            width={128}
+                            height={128}
+                            priority={60}>
+                        </Image>
+                    </div>
+                    <p className={styles.p}><b>AveTemp</b></p>
+                    <p className={styles.small_p}>{avetemp_download_description}</p>
+                    {timer ? '' : <Link href={avetemp_download_link}>{download}</Link>}
                 </div>
-            }
+                <div className={styles.item}>
+                    <div className={styles.image}>
+                        <Image alt={'AveWall fullsize logo'} 
+                            src={'/images/avewall_logo.png'} 
+                            width={128}
+                            height={128}
+                            priority={60}>
+                        </Image>
+                    </div>
+                    <p className={styles.p}><b>AveWall</b></p>
+                    <p className={styles.small_p}>{avewall_download_description}</p>
+                    {timer ? '' : <Link href={avewall_download_link}>{download}</Link>}
+                </div>
+            </div>
+            <p className={styles.links}>
+                { timer ? link_will_be_available + timer + seconds_text : download_disclaimer }
+            </p> 
+
             <div className={styles.ad} id="yandex_rtb_R-A-2952455-3"></div>
             <Script id="yandex_rtb_R-A-2952455-3" strategy="afterInteractive">
                         {`window.yaContextCb.push(()=>{
--- elements/ModalWindow/ModalWindow.js
+++ elements/ModalWindow/ModalWindow.js
@@ -1,10 +1,12 @@
-import styles from './ModalWindow.module.scss'
-import { useSelector } from 'react-redux'
 import Blur from '../../elements/Blur/Blur'
-import { useDispatch } from 'react-redux'
-import { changeIsOpened } from "../../store/features/modalWindowSlice"
+import Image from 'next/image'
 
+import { useDispatch } from 'react-redux'
+import { useSelector } from 'react-redux'
+import { changeIsOpened } from "../../store/features/modalWindowSlice"
 import { selectIsOpened } from '../../store/features/modalWindowSlice'
+
+import styles from './ModalWindow.module.scss'
 
 export default function ModalWindows(props) {
     const isOpened = useSelector(selectIsOpened);
@@ -19,7 +21,17 @@ export default function ModalWindows(props) {
         <>
             <Blur isActive={isOpened} onClick={closeWindow} zIndex={99}/> 
             <div className={isOpened ? styles.wrapper : styles.hidden}>
-                <div className={styles.header}>{props.title}</div>
+                <div className={styles.header}>
+                    <div className={styles.header}>{props.title}</div>
+                    <div className={styles.close} onClick={closeWindow}>
+                        <Image alt={'Close donwload window'} 
+                                src={'/images/close_icon.png'} 
+                                width={20}
+                                height={20}
+                                priority={60}>
+                        </Image>
+                    </div>
+                </div>
                 {props.children}
             </div>
         </>
